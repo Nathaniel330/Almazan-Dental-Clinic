6b320ebf4b439d366998ffeabdcb3c521a3ac12c
Nathaniel330 <nathaniel.valdez.soriano@gmail.com>
2022-09-18 14:22:15 +0800

    remove 'server.listen' in app.js-var port_number... removed useless slideshow code in home.ejs

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set("views", "render")
 //     console.log(`Connected to port: ${port}`)
 // })
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = process.env.PORT || 3000;
 app.listen(port_number);
 
 
