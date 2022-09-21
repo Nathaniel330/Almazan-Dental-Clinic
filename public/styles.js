2f8a71b2f6d643a20821f9f389155339bd79879a
Nathaniel330 <nathaniel.valdez.soriano@gmail.com>
2022-09-22 03:51:06 +0800

    Frontend Complete -test

--- public/styles.js
+++ public/styles.js
@@ -1,3 +1,72 @@
+// ------------ Home Banner Switcher
+var next = document.getElementById("next");
+var prev = document.getElementById("prev");
+var banner = document.getElementById("banner_home");
+
+
+var bgImage = new Array(
+    "./img/bamboobrush.jfif",
+    "./img/instruments.jfif",
+    "./img/dentaldrills.jfif",
+    "./img/dentist.jfif",
+    "./img/comparing.png",
+    "./img/handslove.jfif",
+    "./img/dentalconcept.jfif"
+)
+let i = 0;
+
+next.onclick = ()=> {
+    console.log(i)
+    if (i < bgImage.length - 1) {
+        banner.style.backgroundImage = "url('"+bgImage[i + 1]+"')";
+        i++;
+    } else {
+        i = 0; 
+        banner.style.backgroundImage = "url('"+bgImage[i]+"')";
+    }
+    console.log(i)
+}
+
+prev.onclick = ()=> {
+    if (i > 0 ) {
+        banner.style.backgroundImage = "url('"+bgImage[i - 1]+"')";
+        i--;
+    } else {
+        i = bgImage.length - 1;
+        banner.style.backgroundImage = "url('"+bgImage[i]+"')";
+    }
+}
+
+bannerSwitchAuto();
+
+function bannerSwitchAuto() {
+  i++;
+  if (i > bgImage.length - 1 ) {i = 0}
+  banner.style.backgroundImage = "url('"+bgImage[i]+"')";
+  setTimeout(bannerSwitchAuto, 7000);
+}
+
+
+
+// ------------ Home Slideshow @ XS - S screen view
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+
+
+// ------------ Home Slideshow @ 768px (medium screen view)
 let assetIndex = 1;
 displayAuto(assetIndex);
   
